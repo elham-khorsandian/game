93c81c8678436cd838797c598cc03c91afd62dff
Eli Kh <elikh@Elis-MBP.fritz.box>
2022-05-04 16:22:37 +0200

    6

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
-const previousButton = document.getElementById('previous')
-const nextButton = document.getElementById('next')
-const questionContainer = document.getElementById('question-container')
+//const previousButton = document.getElementById('previous')
+//const nextButton = document.getElementById('next')
+/* const questionContainer = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
-const answers = document.getElementById('answers')
+const answers = document.getElementById('answers') */
 
+const question1 = document.getElementById("question1");
+const deutsche = document.getElementById("deutsche");
+const commerz = document.getElementById("commerz");
+const question2 = document.getElementById("question2");
+const frankfurt = document.getElementById("frankfurt");
+const berlin = document.getElementById("berlin");
+const submit = document.getElementById("submit")
+let score = 0
+
+deutsche.addEventListener("click", () => {
+    deutsche.textContent = "correct"
+    score +=1  
+});
+commerz.addEventListener("click", () => {
+    commerz.textContent = "wrong" 
+});
+
+frankfurt.addEventListener("click" , () =>{
+    frankfurt.textContent = "correct"
+    score +=1
+})
+berlin.addEventListener("click", () => {
+    berlin.textContent = "wrong" 
+});
+
+submit.addEventListener("click" , () => {
+    submit.textContent = score
+})
+
+
+
+
+
+
+
+/* const questions = [
+    {question: 'where do you work?', answers: [
+        {text: 'Deutsche Bakn' , correct: true},
+        {text: 'Commerz Bank' , correct : false}
+    ]},
+    {question: 'where are the headquartes of DB located?', answers: [
+        {text : 'Frankfurt' , correct: true} ,
+        {text: ' Berlin', correct:false}
+    ]}
+] */
+
+
+/* 
 nextButton.addEventListener('click' , () => {
     currentQuestion++
     setNextQuestion()
@@ -27,16 +75,35 @@ function showQuestion(questions){
         
     });
 }
- 
 
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+  }
+  
+  function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answers.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
 
-const questions = [
-    {question: 'where do you work?', answers: [
-        {text: 'Deutsche Bakn' , correct: true},
-        {text: 'Commerz Bank' , correct : false}
-    ]},
-    {question: 'where are the headquartes of DB located?', answers: [
-        {text : 'Frankfurt' , correct: true} ,
-        {text: ' Berlin', correct:false}
-    ]}
-]
+ */
