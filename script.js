2d74f6b5571c39fb37849cf7772e6ca1a618ad48
Eli Kh <elikh@Elis-MBP.fritz.box>
2022-05-04 13:55:48 +0200

    4

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const question
+const previousButton = document.getElementById('previous')
+const nextButton = document.getElementById('next')
+const question = document.getElementById('question')
+const answers = document.getElementById('answers')
+
+nextButton.addEventListener('click' , () => {
+    currentQuestion++
+    setNextQuestion()
+})
+
+const questions = [
+    {question: 'where do you work?', answers: [
+        {text: 'Deutsche Bakn' , correct: true},
+        {text: 'Commerz Bank' , correct : false}
+    ]},
+
+]
