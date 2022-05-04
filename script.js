507cbe67b77ee6af6fd272e078400bfb8fc7d0eb
Eli Kh <elikh@Elis-MBP.fritz.box>
2022-05-04 16:48:30 +0200

    7

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//const previousButton = document.getElementById('previous')
-//const nextButton = document.getElementById('next')
-/* const questionContainer = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answers = document.getElementById('answers') */
 
 const question1 = document.getElementById("question1");
 const deutsche = document.getElementById("deutsche");
@@ -16,94 +11,28 @@ let score = 0
 deutsche.addEventListener("click", () => {
     deutsche.textContent = "correct"
     score +=1  
+    deutsche.style.color = "green"
 });
 commerz.addEventListener("click", () => {
     commerz.textContent = "wrong" 
+    commerz.style.color = "red"
 });
 
 frankfurt.addEventListener("click" , () =>{
     frankfurt.textContent = "correct"
     score +=1
+    frankfurt.style.color = "green"
 })
 berlin.addEventListener("click", () => {
     berlin.textContent = "wrong" 
+    berlin.style.color = "red"
 });
 
 submit.addEventListener("click" , () => {
-    submit.textContent = score
+    submit.textContent = ("Your score is = " + score)
 })
 
 
 
 
 
-
-
-/* const questions = [
-    {question: 'where do you work?', answers: [
-        {text: 'Deutsche Bakn' , correct: true},
-        {text: 'Commerz Bank' , correct : false}
-    ]},
-    {question: 'where are the headquartes of DB located?', answers: [
-        {text : 'Frankfurt' , correct: true} ,
-        {text: ' Berlin', correct:false}
-    ]}
-] */
-
-
-/* 
-nextButton.addEventListener('click' , () => {
-    currentQuestion++
-    setNextQuestion()
-})
-
-function setNextQuestion() {
-    resetState()
-    showQuestion(questions[currentQuestion])
-}
-function showQuestion(questions){
-    questionElement.innerText=questions.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if(answer.correct) {
-            button.dataset.correct= answer.correct
-        }
-        button.addEventListener('click' , selectAnswer)
-        answers.appendChild(button)
-        
-    });
-}
-
-function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
-  }
-  
-  function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answers.children).forEach(button => {
-      setStatusClass(button, button.dataset.correct)
-    })
-  
-  function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-      element.classList.add('correct')
-    } else {
-      element.classList.add('wrong')
-    }
-  }
-  
-  function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-  }
-
- */
